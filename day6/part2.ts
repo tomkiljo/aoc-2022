4032b3aed2df6fb54620f9aed5e036d4f1bf834a
Tom Kiljo <tom.kiljo@nitor.com>
2022-12-06 07:41:51 +0200

    add day6 solutions

---
+++ day6/part2.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const input = readFileSync(
+  path.join(path.dirname(fileURLToPath(import.meta.url)), "input.txt"),
+  "utf-8"
+);
+
+for (let i = 13; i < input.length; i++) {
+  const buffer = [...input.slice(i - 13, i + 1)];
+  const different = buffer.every((char, index, arr) => {
+    return arr.indexOf(char, index + 1) < 0;
+  });
+  if (different) {
+    console.log(i + 1);
+    break;
+  }
+}
